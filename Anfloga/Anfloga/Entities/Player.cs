a334d88331d82a8142b21d3fea8083b9cd3595a3
Rick Blaylock <blay4480@gmail.com>
2017-05-15 21:18:28 -0600

    Added an action Icon to the player which updates it's visibility and chain name.

--- Anfloga/Anfloga/Entities/Player.cs
+++ Anfloga/Anfloga/Entities/Player.cs
@@ -46,6 +46,8 @@ namespace Anfloga.Entities
 
         private Vector2 lastCheckpointPosition;
 
+        private float actionIconOffset;
+
         #endregion
 
         #region Initialize
@@ -67,7 +69,21 @@ namespace Anfloga.Entities
             InitializeCollidingObjectList();
 
             InitializeEmitter();
+
+            InitializeAcionIcon();
 		}
+
+        private void InitializeAcionIcon()
+        {
+            //Possible bug? The pixel coorinates are not updating on initialize.
+            ActionIconInstance.VisualName = "CollectMineral";
+            ActionIconInstance.VisualName = "PurchaseCollector";
+            
+            actionIconOffset = AxisAlignedRectangleInstance.Height / 2;
+
+            ActionIconInstance.RelativeY = actionIconOffset + ActionIconInstance.SpriteInstanceHeight;
+            ActionIconInstance.Visible = false;
+        }
 
         private void InitializeEmitter()
         {
@@ -129,6 +145,8 @@ namespace Anfloga.Entities
 
         private void CustomActivity()
 		{
+            UpdateActionIconActivity();
+
             PerformMovementInput();
 
             PerformActionInput();
@@ -141,6 +159,29 @@ namespace Anfloga.Entities
             //Perform hud update at the end. Incase we have abilities that consume oxygen.
             UpdateHudActivity();
 		}
+
+        private void UpdateActionIconActivity()
+        {
+            if(ObjectsToPerformCurrencyTransactionOn.Count > 0)
+            {
+                ActionIconInstance.Visible = true;
+
+                if(ObjectsToPerformCurrencyTransactionOn[0] is MineralDeposit)
+                {
+                    ActionIconInstance.VisualName = "CollectMineral";
+                }
+                else
+                {
+                    ActionIconInstance.VisualName = "PurchaseCollector";
+                }
+
+                ActionIconInstance.RelativeY = actionIconOffset + ActionIconInstance.SpriteInstanceHeight / 2;
+            }
+            else
+            {
+                ActionIconInstance.Visible = false;
+            }
+        }
 
         private void PerformActionInput()
         {
