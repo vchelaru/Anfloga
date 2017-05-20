3ede7ebc402dbe3689c22fceaf61bec110093f18
Justin Johnson <justin.d.johnson@gmail.com>
2017-05-20 13:28:37 -0600

    Major overhaul of tiles with implementation of terrain tools.

--- Anfloga/Anfloga/Setup/CameraSetup.cs
+++ Anfloga/Anfloga/Setup/CameraSetup.cs
@@ -9,12 +9,12 @@
 			internal static void ResetCamera (Camera cameraToReset)
 			{
 				FlatRedBall.Camera.Main.Orthogonal = true;
-				FlatRedBall.Camera.Main.OrthogonalHeight = 540;
-				FlatRedBall.Camera.Main.OrthogonalWidth = 960;
+				FlatRedBall.Camera.Main.OrthogonalHeight = 270;
+				FlatRedBall.Camera.Main.OrthogonalWidth = 480;
 				FlatRedBall.Camera.Main.FixAspectRatioYConstant();
 				SetAspectRatioTo(16 / 9m);
 			}
-			internal static void SetupCamera (Camera cameraToSetUp, Microsoft.Xna.Framework.GraphicsDeviceManager graphicsDeviceManager, int width = 960, int height = 540)
+			internal static void SetupCamera (Camera cameraToSetUp, Microsoft.Xna.Framework.GraphicsDeviceManager graphicsDeviceManager, int width = 480, int height = 270)
 			{
 				#if WINDOWS || DESKTOP_GL
 				FlatRedBall.FlatRedBallServices.Game.Window.AllowUserResizing = true;
