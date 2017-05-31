d36f45156887ae6f6c54b91e36bb84e12db78dda
David Antognoli <david.antognoli@gmail.com>
2017-05-31 00:03:15 -0500

    removed some old levels revised ending screen

--- Anfloga/Anfloga/Screens/EndingScreen.cs
+++ Anfloga/Anfloga/Screens/EndingScreen.cs
@@ -21,14 +21,16 @@ namespace Anfloga.Screens
 
 		void CustomInitialize()
 		{
-            if(GlobalData.TotalCurrencyCollected > 10)
-            {
-                EndingScreenGumRuntime.EndingText = "You got so much!";
-            }
-            else
-            {
-                EndingScreenGumRuntime.EndingText = $"You so poor!\nCrystals Found: {GlobalData.TotalCurrencyCollected}";
-            }
+            EndingScreenGumRuntime.MineralCountText = "Minerals Obtained: " + GlobalData.TotalCurrencyCollected;
+            EndingScreenGumRuntime.EndingText = "You made it to the surface. You wish to share the secrets of Anfloga to prevent others from falling into this death trap. \n\nYou are resourceful. You fashion a distress beacon from parts of your ship. You hope a nearby trader notices it before the Mining Corporation sends a cleanup crew. \n\nIn the meantime, you hone your fishing skills.";
+            //if(GlobalData.TotalCurrencyCollected > 10)
+            //{
+            //    EndingScreenGumRuntime.EndingText = "You made it to the surface. You wish to share the secrets of Anfloga to prevent others from falling into this death trap. \n\nYou are resourceful. You fashion a distress beacon from parts of your ship. You hope a nearby trader notices it before the Mining Corporation sends a cleanup crew. \n\nIn the meantime, you hone your fishing skills.";
+            //}
+            //else
+            //{
+            //    EndingScreenGumRuntime.EndingText = $"You so poor!\nCrystals Found: {GlobalData.TotalCurrencyCollected}";
+            //}
         }
 
 		void CustomActivity(bool firstTimeCalled)
