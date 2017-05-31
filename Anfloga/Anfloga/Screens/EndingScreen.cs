53d7775153a57537e28219995b79b8ab1c41381b
David Antognoli <david.antognoli@gmail.com>
2017-05-31 15:56:06 -0500

    added another ending and fixed the player start.

--- Anfloga/Anfloga/Screens/EndingScreen.cs
+++ Anfloga/Anfloga/Screens/EndingScreen.cs
@@ -22,15 +22,16 @@ namespace Anfloga.Screens
 		void CustomInitialize()
 		{
             EndingScreenGumRuntime.MineralCountText = "Minerals Obtained: " + GlobalData.TotalCurrencyCollected;
-            EndingScreenGumRuntime.EndingText = "You made it to the surface. You wish to share the secrets of Anfloga to prevent others from falling into this death trap. \n\nYou are resourceful. You fashion a distress beacon from parts of your ship. You hope a nearby trader notices it before the Mining Corporation sends a cleanup crew. \n\nIn the meantime, you hone your fishing skills.";
-            //if(GlobalData.TotalCurrencyCollected > 10)
-            //{
-            //    EndingScreenGumRuntime.EndingText = "You made it to the surface. You wish to share the secrets of Anfloga to prevent others from falling into this death trap. \n\nYou are resourceful. You fashion a distress beacon from parts of your ship. You hope a nearby trader notices it before the Mining Corporation sends a cleanup crew. \n\nIn the meantime, you hone your fishing skills.";
-            //}
-            //else
-            //{
-            //    EndingScreenGumRuntime.EndingText = $"You so poor!\nCrystals Found: {GlobalData.TotalCurrencyCollected}";
-            //}
+            
+
+            if (GlobalData.TotalCurrencyCollected > 60)
+            {
+                EndingScreenGumRuntime.EndingText = "You made it to the surface. You wish to share the secrets of Anfloga to prevent others from falling into this death trap. \n\nYou are resourceful. You fashion a distress beacon from parts of your ship. You hope a nearby trader notices it before the Mining Corporation sends a cleanup crew. \n\nIn the meantime, you hone your fishing skills.";
+            }
+            else
+            {
+                EndingScreenGumRuntime.EndingText = "Thank you for completing your contract with Anfloga Mining Corporation. \n\nUnfortunately, we have deciced against commuting your sentence, an option we retain as stated in Clause 6B. Perhaps if you had gathered more minerals, the situation would be different. \n\nPlease wait patiently and avoid hiding from the Anfloga Mining Corporation cleanup crew.";
+            }
         }
 
 		void CustomActivity(bool firstTimeCalled)
